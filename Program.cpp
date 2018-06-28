5b4f06c378ecd52111c0ef07f9be3563a98e357d
Ryan Justin <ryan.justin@b2be.com>
2018-06-28 13:45:25 +0800

    modify program.cpp

--- Program.cpp
+++ Program.cpp
@@ -8,5 +8,7 @@ int main()
 	cout << "Added a second line" << endl;
 cout << "Added a third line" <<endl;	
 cout << "this line will cause conflict" <<endl;
+cout <<"this is a fifth line" <<endl;
+cout<<"added a line to show it is pushed to github"<<endl;
 	return 0;
 }
